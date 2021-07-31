d8e67f3f921bb67ca78afca9c5c934d71499cd7a
Kevin Mazats <k.mazats@gmail.com>
2021-07-31 23:37:25 +0200

    deleted custom admin frontend
    focused on using strapi functionnalities instead

--- frontend-admin/src/UserContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext, Dispatch, SetStateAction } from "react";
-interface User {
-	user: null | object;
-	setUser?: Dispatch<SetStateAction<object | null >>
-}
-export const UserContext = createContext<User>({ user: null });
